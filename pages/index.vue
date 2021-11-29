2cad90a386b7587fc46c1cac719144edfd9e17d8
Jabelic <hibiki990523@gmail.com>
2021-11-30 01:50:12 +0900

    ADD: mathquill into plugins

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6"> </v-col>
+  </v-row> -->
+  <div id="index">
+    <Mathquill />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
---
+++ templates/Mathquill.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="index" class="d-flex">hogehuga</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
